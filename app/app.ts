cd0c0e4b2bc2c2c8a89c74781e9e585788754030
Pankaj Kolhe <Pankaj Kolhe>
2017-07-19 22:02:11 +0530

    Changes added

--- app/app.ts
+++ app/app.ts
@@ -6,3 +6,18 @@ function startGame() {
 }
 
 document.getElementById('startGame').addEventListener('click', startGame);
+
+/* Below 2 lines will execute and gives output 10 in console with var */
+console.log(no);
+var no = 10;
+
+/* Below 2 lines will not compile, compile time error with var */
+//console.log(someString);
+//let someString = "Pankaj";
+
+/* Below 2 lines will execute and gives output Pankaj in console with let and const */
+let someString = "Pankaj";
+console.log(someString);
+
+const someName = "PankajKolhe";
+console.log(someName);
--- js/app.js
+++ js/app.js
@@ -4,4 +4,10 @@ function startGame() {
     console.log("Starting new Game....");
 }
 document.getElementById('startGame').addEventListener('click', startGame);
+console.log(no);
+var no = 10;
+var someString = "Pankaj";
+console.log(someString);
+var someName = "PankajKolhe";
+console.log(someName);
 //# sourceMappingURL=app.js.map
