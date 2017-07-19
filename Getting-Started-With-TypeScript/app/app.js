3d2cca1ae3ce11d1e1610c8c2104d8f2dc56905a
Pankaj Kolhe <Pankaj Kolhe>
2017-07-19 21:44:23 +0530

    Configuration done

--- Getting-Started-With-TypeScript/app/app.js
+++
@@ -1,6 +0,0 @@
-function startGame() {
-    // Starting a new Game
-    var messageElement = document.getElementById('messages');
-    messageElement.innerText = "Welcome to MultiMath ! Starting a new Game ...";
-}
-document.getElementById('startGame').addEventListener('click', startGame);
